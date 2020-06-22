5bc0d2a8b767af36cf3cc3b3c07cced282916917
Ataor <slyguggi@gmail.com>
2020-06-22 10:28:18 +0200

    4

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -1,52 +1,63 @@
 ﻿using UnityEngine;
 
-public class HexCell : MonoBehaviour {
+public class HexCell : MonoBehaviour
+{
+    public HexCoordinates coordinates;
 
-	public HexCoordinates coordinates;
+    public Color color;
 
-	public Color color;
+    public RectTransform uiRect;
 
-	public RectTransform uiRect;
+    public int Elevation
+    {
+        get { return elevation; }
+        set
+        {
+            elevation = value;
+            Vector3 position = transform.localPosition;
+            position.y = value * HexMetrics.elevationStep;
+            position.y +=
+                (HexMetrics.SampleNoise(position).y * 2f - 1f) *
+                HexMetrics.elevationPerturbStrength;
+            transform.localPosition = position;
 
-	public int Elevation {
-		get {
-			return elevation;
-		}
-		set {
-			elevation = value;
-			Vector3 position = transform.localPosition;
-			position.y = value * HexMetrics.elevationStep;
-			transform.localPosition = position;
+            Vector3 uiPosition = uiRect.localPosition;
+            uiPosition.z = -position.y;
+            uiRect.localPosition = uiPosition;
+        }
+    }
 
-			Vector3 uiPosition = uiRect.localPosition;
-			uiPosition.z = elevation * -HexMetrics.elevationStep;
-			uiRect.localPosition = uiPosition;
-		}
-	}
+    public Vector3 Position
+    {
+        get { return transform.localPosition; }
+    }
 
-	int elevation;
+    int elevation;
 
-	[SerializeField]
-	HexCell[] neighbors;
+    [SerializeField] HexCell[] neighbors;
 
-	public HexCell GetNeighbor (HexDirection direction) {
-		return neighbors[(int)direction];
-	}
+    public HexCell GetNeighbor(HexDirection direction)
+    {
+        return neighbors[(int) direction];
+    }
 
-	public void SetNeighbor (HexDirection direction, HexCell cell) {
-		neighbors[(int)direction] = cell;
-		cell.neighbors[(int)direction.Opposite()] = this;
-	}
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int) direction] = cell;
+        cell.neighbors[(int) direction.Opposite()] = this;
+    }
 
-	public HexEdgeType GetEdgeType (HexDirection direction) {
-		return HexMetrics.GetEdgeType(
-			elevation, neighbors[(int)direction].elevation
-		);
-	}
+    public HexEdgeType GetEdgeType(HexDirection direction)
+    {
+        return HexMetrics.GetEdgeType(
+            elevation, neighbors[(int) direction].elevation
+        );
+    }
 
-	public HexEdgeType GetEdgeType (HexCell otherCell) {
-		return HexMetrics.GetEdgeType(
-			elevation, otherCell.elevation
-		);
-	}
+    public HexEdgeType GetEdgeType(HexCell otherCell)
+    {
+        return HexMetrics.GetEdgeType(
+            elevation, otherCell.elevation
+        );
+    }
 }
